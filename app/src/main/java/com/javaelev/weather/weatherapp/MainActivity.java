cdc6d7173780c6de32ac24e7296859af2c6376ca
javaelev90 <37899614+javaelev90@users.noreply.github.com>
2018-06-02 23:33:21 +0200

    base functionality is done, started working on search bar

--- app/src/main/java/com/javaelev/weather/weatherapp/MainActivity.java
+++ app/src/main/java/com/javaelev/weather/weatherapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.javaelev.weather.weatherapp;
 
+import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -7,7 +9,12 @@ import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,7 +25,6 @@ import com.javaelev.weather.weatherapp.handlers.ListAdapter;
 import com.javaelev.weather.weatherapp.model.ForecastItem;
 import com.squareup.picasso.Picasso;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,15 +49,8 @@ public class MainActivity extends AppCompatActivity {
                     ListView listView = findViewById(R.id.forecastList);
                     ListAdapter adapter = new ListAdapter(getApplicationContext(), R.layout.list_view_row, new ArrayList<ForecastItem>());
                     listView.setAdapter(adapter);
-                    String url = "https://www.yr.no/place/Sweden/Norrbotten/Lule%C3%A5/forecast.xml";
-                    //                    ForecastXMLParser forecastRetriever = new ForecastXMLParser();
-//                    forecastRetriever.getForecast(url);
-//                    LocalDateTime forecastTime, String windDirection, float degreesCelsius, float windSpeed, String symbolCode, String symbolMeaning
-                    OkHttpClient client = new OkHttpClient();
-//                    ForecastCallback forecastCallback = new ForecastCallback(adapter);
 
-                    Request request = new Request.Builder().url(url).build();
-
+                    //This handler is needed to let UI thread re-draw list view after async YR-api call has returned
                     Handler handler = new Handler(Looper.getMainLooper()) {
                         @Override
                         public void handleMessage(Message msg) {
@@ -61,8 +60,9 @@ public class MainActivity extends AppCompatActivity {
                             TextView desc = findViewById(R.id.header_symbol_description);
                             ImageView symbol = findViewById(R.id.header_symbol);
                             ForecastItem firstItem = forecast.get(0);
-                            time.setText(firstItem.getForecastTime().toString());
-                            degrees.setText(firstItem.getDegreesCelsius()+"\u00b0 C");
+
+                            time.setText("Today at: "+firstItem.getForecastTime().toLocalTime().toString());
+                            degrees.setText((int)firstItem.getDegreesCelsius()+"\u00b0 C");
                             desc.setText(firstItem.getSymbolMeaning());
                             String symbolUrl = "http://symbol.yr.no/grafikk/sym/b200/"+firstItem.getSymbolCode()+".png";
                             Picasso.get().load(symbolUrl).into(symbol);
@@ -71,31 +71,14 @@ public class MainActivity extends AppCompatActivity {
                             adapter.notifyDataSetChanged();
                         }
                     };
+
+                    String url = "https://www.yr.no/place/Sweden/Norrbotten/Lule%C3%A5/forecast.xml";
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url(url).build();
                     if(adapter.getCount() == 0){
                         client.newCall(request).enqueue(new ForecastCallback(handler));
                     }
 
-
-
-
-
-
-
-
-
-//                    List<ForecastItem> forecast = new ArrayList<>();
-//                    ForecastItem forecastItem = new ForecastItem(LocalDateTime.now(), "North West", 23, 1.1f, "02d", "Cloudy");
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//                    forecast.add(forecastItem);
-//
-//                    listView.setAdapter(new ListAdapter(getApplicationContext(), R.layout.list_view_row, forecast));
                     return true;
                 case R.id.navigation_dashboard:
                     //mTextMessage.setText(R.string.title_dashboard);
@@ -114,10 +97,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_home);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+                final String[] COUNTRIES = new String[] { "Belgium",
+                "France", "France_", "Italy", "Germany", "Spain" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.editText1);
+        textView.setAdapter(adapter);
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setDisplayShowCustomEnabled(true);
+//        // actionBar.setDisplayShowTitleEnabled(false);
+//        // actionBar.setIcon(R.drawable.ic_action_search);
+//
+//        LayoutInflater inflator = (LayoutInflater) this
+//                .getSystemService(getApplicationContext().LAYOUT_INFLATER_SERVICE);
+//        View v = inflator.inflate(R.layout.actionbar, null);
+//
+//        actionBar.setCustomView(v);
+//        final String[] COUNTRIES = new String[] { "Belgium",
+//                "France", "France_", "Italy", "Germany", "Spain" };
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+//        AutoCompleteTextView textView = (AutoCompleteTextView) v
+//                .findViewById(R.id.editText1);
+//        textView.setAdapter(adapter);
+
 //        FileLoader fileLoader = new FileLoader();
 //        fileLoader.makeCountryCodesHashMap(getResources());
 
--- app/src/main/java/com/javaelev/weather/weatherapp/handlers/ListAdapter.java
+++ app/src/main/java/com/javaelev/weather/weatherapp/handlers/ListAdapter.java
@@ -1,9 +1,7 @@
 package com.javaelev.weather.weatherapp.handlers;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +14,8 @@ import com.javaelev.weather.weatherapp.R;
 import com.javaelev.weather.weatherapp.model.ForecastItem;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class ListAdapter extends ArrayAdapter<ForecastItem>{
 
@@ -60,19 +57,10 @@ public class ListAdapter extends ArrayAdapter<ForecastItem>{
         String symbolUrl = context.getText(R.string.symbolURL)+forecastItem.getSymbolCode()+".png";
         Picasso.get().load(symbolUrl).into(listItemViewHolder.symbol);
         listItemViewHolder.symbolMeaning.setText(forecastItem.getSymbolMeaning());
-        listItemViewHolder.dateTime.setText(forecastItem.getForecastTime().toString());
-        listItemViewHolder.degrees.setText(forecastItem.getDegreesCelsius()+"\u00b0 C");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM 'at:' HH:mm");
+        listItemViewHolder.dateTime.setText(forecastItem.getForecastTime().format(formatter).toString());
+        listItemViewHolder.degrees.setText((int)forecastItem.getDegreesCelsius()+"\u00b0 C");
 
         return convertView;
     }
-
-//    public void addAll(ForecastItem... forecastItems){
-//        IntStream.range(0, forecastItems.length).forEach(i -> this.forecastItems.add(forecastItems[i]));
-//        notifyDataSetChanged();
-//
-//    }
-//
-//    public int getCount(){
-//        return forecastItems.size();
-//    }
 }
